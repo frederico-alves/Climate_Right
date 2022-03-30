8619ffc787ce42b5ea301ee42bef9e040594f419
RaniaZarka <raniazarka@hotmail.com>
2022-03-30 20:33:28 +0200

    Ionic form and pages generated

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActionSheetController, AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-form',
   templateUrl: './form.page.html',
   styleUrls: ['./form.page.scss'],
 })
 export class FormPage implements OnInit {
+unit:boolean;
 
-  constructor() { }
+constructor( private actionSheetCtrl: ActionSheetController,
+  private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+ /* async presentActionSheet(){
+    const actionSheet = await this.actionSheetCtrl.create({
+    
+      }
+    );
+  }*/
+  async presentAlertSheet(){
+    const alert = await this.alertCtrl.create({
+      message:"How do you feel the temperature in your zone right now <br> Note: Temperature in this room can vary between 18.5°C and 21.5°C",
+      buttons :[{ text :'ok', role :'cancel'}]
+    });
+    await alert.present();
+  }
 }
