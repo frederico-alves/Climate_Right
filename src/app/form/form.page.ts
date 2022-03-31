d40b4e2d2b3da8b745e3a3c02f19c873d6b4a5b4
Akritibali2025 <baliakriti6@gmail.com>
2022-03-31 22:06:45 +0200

    informationbutonsworking

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -24,14 +24,27 @@ constructor( private actionSheetCtrl: ActionSheetController,
       }
     );
   }*/
-  async presentAlertSheet(){
+  async presentAlertSheetTemperature(){
     const alert = await this.alertCtrl.create({
       message:"How do you feel the temperature in your zone right now <br> Note: Temperature in this room can vary between 18.5°C and 21.5°C",
       buttons :[{ text :'ok', role :'cancel'}]
     });
     await alert.present();
   }
-
+  async presentAlertSheetAirQuality(){
+    const alert = await this.alertCtrl.create({
+      message:"How do you feel the polution level in your zone right now?<br>Very tired: the polution is high, you feel sleepy, tired.<br> Very energetic: the polution is low, you feel fresh and full of energy.<br>To give a good evaluation, lack of sleep or excessive physical activities should not be counted as factors of tiredness.",
+      buttons :[{ text :'ok', role :'cancel'}]
+    });
+    await alert.present();
+  }
+  async presentAlertSheetHumidity(){
+    const alert = await this.alertCtrl.create({
+      message:"How do you feel the humidity in your zone right now?<br>Very dry: you feel your throat is dry.<br> Very muggy: clammy skin.<br>Note: Humidity in this room can vary between 60% and 80%",
+      buttons :[{ text :'ok', role :'cancel'}]
+    });
+    await alert.present();
+  }
   async showMyToast() {
     const toast = await this.toastController.create({
       message: 'Your settings have been saved',
@@ -45,3 +58,4 @@ constructor( private actionSheetCtrl: ActionSheetController,
   
 
 }
+
